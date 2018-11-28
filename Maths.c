294118c85e0757910f386b652e1e7c4312d2842d
Katie Baker [el16ecrb] <el16ecrb@leeds.ac.uk>
2018-11-28 15:52:55 +0000

    Finished Maths

--- Maths.c
+++ Maths.c
@@ -8,28 +8,53 @@
 float consts[] = {pi,e};
 
 ////        arg1   arg2
-float  sind(float,float); // 0 sin(arg1)
-float asind(float,float); // 1 asin(arg1)
-float  fact(float,float); // 2 arg1!
-float  cosd(float,float); // 3 cos(arg1)
-float acosd(float,float); // 4 acos(arg1)
-float  tand(float,float); // 5 tan(arg1)
-float atand(float,float); // 6 atan(arg1)
-float  root(float,float); // 7 arg1 ^ ( 1 / arg2)
-float sqrtd(float,float); // 8 arg1 ^ (1 / 2)
-float  logd(float,float); // 9 log arg1 (arg2)
-float  divd(float,float); //10 arg1 / arg2
-float  mult(float,float); //11 arg1 * arg2
-float  plus(float,float); //12 arg1 + arg2
-float minus(float,float); //13 arg1 - arg2
+float  sind(float,float); // sin(arg1)
+float asind(float,float); // asin(arg1)
+float  fact(float,float); // arg1!
+float  cosd(float,float); // cos(arg1)
+float acosd(float,float); // acos(arg1)
+float  tand(float,float); // tan(arg1)
+float atand(float,float); // atan(arg1)
+float  root(float,float); // arg1 ^ ( 1 / arg2)
+float sqrtd(float,float); // arg1 ^ (1 / 2)
+float  logd(float,float); // log arg1 (arg2)
+float  powd(float,float); // arg1 ^ arg2
+float   neg(float,float); // -arg1
+float  divd(float,float); // arg1 / arg2
+float  mult(float,float); // arg1 * arg2
+float  plus(float,float); // arg1 + arg2
+float minus(float,float); // arg1 - arg2
+float noop(float,float);  // NAN
 
-float (*ops[])(float arg1, float arg2) = {sind,asind,fact,cosd,acosd,tand,atand,root,sqrtd,logd,divd,mult,plus,minus};
+typedef float (*ops)(float arg1, float arg2);
 
-float maths(int op,float arg1, float arg2) {
+ops functionLookup(char op){
+	switch(op){
+			case 's': return sind;
+			case 'd': return asind;
+			case '!': return fact;
+			case 'c': return cosd;
+			case 'v': return acosd;
+			case 't': return tand; 
+			case 'y': return atand;
+			case 'r': return root;
+			case 'l': return logd;
+			case '/': return divd;
+			case '*': return mult;
+			case '+': return plus;
+			case '-': return minus;
+		  case '^': return powd;
+		  case 'n': return neg;
+		  case '_': return noop;
+			default: return noop;
+		}
+}
+
+float maths(char op,float arg1, float arg2) {
 
 	static float ans;
 	
-	ans = ops[op](arg1,arg2);
+	ans = functionLookup(op)(arg1,arg2);
 	
 	if (isinf(ans)){ // If the answer is infinite set it to 'not a number'
 		ans = NAN;
@@ -40,6 +65,10 @@ float maths(int op,float arg1, float arg2) {
 }
 float constants(int element){
 	return consts[element];
+}
+
+float noop(float arg1, float arg2){
+	return NAN;
 }
 
 float sind(float arg1, float arg2){
@@ -86,15 +115,26 @@ float logd(float arg1, float arg2){
 	return (logf(arg2)/logf(arg1));
 }
 
+float  powd(float arg1, float arg2){
+	return (powf(arg1,arg2));
+}
+
+float   neg(float arg1, float arg2){
+	return (-arg1);
+}
+
 float divd(float arg1,float arg2){
 	return (arg1 / arg2);
 }
+
 float  mult(float arg1,float arg2){
 	return (arg1 * arg2);
 }
+
 float  plus(float arg1,float arg2){
 	return (arg1 + arg2);
 }
+
 float minus(float arg1,float arg2){
 	return (arg1 - arg2);
 }
